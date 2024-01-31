c7737c13ef2cf72ff3d667ed0a55c993bd58e8de
Vicky(Xinyi) Xiang <xinyixiang1129@gmail.com>
2024-01-31 10:03:29 +0800

    sample rouge script
    plus create D3 directory

---
+++ D3/rouge.py
@@ -0,0 +1,27 @@
+"""
+This is the sample code for implemeneting ROUGE using the ROUGE package.
+"""
+from rouge import Rouge
+
+def calculate_rouge(reference, hypothesis):
+    rouge = Rouge()
+    scores = rouge.get_scores(hypothesis, reference)
+    return scores
+
+# Example usage
+reference_summary = "This is the reference summary."
+hypothesis_summary = "This is the hypothesis summary."
+
+rouge_scores = calculate_rouge(reference_summary, hypothesis_summary)
+
+print("ROUGE-1 Precision:", rouge_scores[0]['rouge-1']['p'])
+print("ROUGE-1 Recall:", rouge_scores[0]['rouge-1']['r'])
+print("ROUGE-1 F1 Score:", rouge_scores[0]['rouge-1']['f'])
+
+print("\nROUGE-2 Precision:", rouge_scores[0]['rouge-2']['p'])
+print("ROUGE-2 Recall:", rouge_scores[0]['rouge-2']['r'])
+print("ROUGE-2 F1 Score:", rouge_scores[0]['rouge-2']['f'])
+
+print("\nROUGE-L Precision:", rouge_scores[0]['rouge-l']['p'])
+print("ROUGE-L Recall:", rouge_scores[0]['rouge-l']['r'])
+print("ROUGE-L F1 Score:", rouge_scores[0]['rouge-l']['f'])
