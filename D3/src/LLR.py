644599171d23c3d8eee9f526669bc56b6f43761e
cl-victor1 <cheng_long_@126.com>
2024-01-31 22:36:33 +0800

    LLR

---
+++ D3/src/LLR.py
@@ -0,0 +1,119 @@
+import math
+import sys
+from collections import defaultdict
+import os
+from decimal import Decimal
+from nltk.corpus import stopwords
+
+def combinations(n, k):
+    # Calculate n! / (k! * (n - k)!)
+    # This approach avoids computing large factorials and mitigates the risk of overflow errors.
+    result = 1
+    for i in range(1, k + 1):
+        result *= (n - i + 1) / i
+    return result
+
+def background_count(back_corpus_file):
+    word_count = defaultdict(int)
+    total_words = 0
+    with open(back_corpus_file, 'r') as back:
+        for line in back:
+            words = line.strip().split()
+            for word in words:
+                total_words += 1
+                word_count[word.lower()] += 1 # ignore the case
+    return total_words, word_count
+         
+def input_count(files_path, stopwords):         
+    word_count = defaultdict(int)
+    total_words = 0
+    files = os.listdir(files_path)
+    all_sentences = []
+    # Iterate through each file
+    for file_name in files:
+        # Create the full path to the file
+        file_path = os.path.join(files_path, file_name)
+        with open(file_path, 'r') as file:
+            for line in file.readlines():
+                # exclude meta information
+                if not line.startswith("HEADLINE") and not line.startswith("DATE_TIME") \
+                    and not line.startswith("DATETIME") and not line.startswith("DATELINE"):
+                    words = line.strip().split()
+                    for word in words:
+                        if stopwords:
+                            if word not in stopwords:
+                                total_words += 1
+                                word_count[word.lower()] += 1 # ignore the case
+                        else:
+                            total_words += 1
+                            word_count[word.lower()] += 1 # ignore the case
+                    if len(words) >= 5: # exclude sentences shorter than 5
+                        all_sentences.append(words)                        
+    return total_words, word_count, all_sentences      
+            
+def LLR(n2, back_word_count, n1, input_word_count, confidence_level):
+    important_words = set()
+    for word in input_word_count.keys():
+        k1 = input_word_count[word]
+        k2 = max(back_word_count[word], 1) # to avoid log(0)
+        p1 = k1 / n1
+        p2 = k2 / n2
+        p = (k1 + k2) / (n1 + n2)
+        # change multiplications into additions of log
+        ratio = 2 * (math.log(Decimal(combinations(n1, k1))) + k1 * math.log(p1) + (n1 - k1) * math.log(1 - p1)\
+            + math.log(Decimal(combinations(n2, k2))) + k2 * math.log(p2) + (n2 - k2) * math.log(1 - p2)\
+            - math.log(Decimal(combinations(n1, k1))) - k1 * math.log(p) - (n1 - k1) * math.log(1 - p)\
+                - math.log(Decimal(combinations(n2, k2))) - k2 * math.log(p) - (n2 - k2) * math.log(1 - p))
+        if float(ratio) > confidence_level:
+            important_words.add(word)
+    return important_words
+        
+def calculate_weight(sentence, important_words): # every sentence is a list of tokenized words
+    weight = 0
+    sentence_length = 0
+    for word in sentence:
+        sentence_length += 1
+        if word in important_words:
+            weight += 1
+    return weight / sentence_length # weight of the sentence
+    
+        
+if __name__ == "__main__":   
+    back_corpus_file = sys.argv[1]
+    input_directory = sys.argv[2]
+    output_directory = sys.argv[3]
+    confidence_level = float(sys.argv[4])
+    summary_length = int(sys.argv[5])
+    if_stopwords = int(sys.argv[6]) # 0 means not using stopwords, 1 means using stopwords
+    
+    back_total_words, back_word_count = background_count(back_corpus_file)
+    english_stopwords = None
+    if if_stopwords == 1:
+        english_stopwords = set(stopwords.words('english'))
+    for subdir in os.listdir(input_directory):
+        files_path = os.path.join(input_directory, subdir)
+        input_total_words, input_word_count, all_sentences = input_count(files_path, english_stopwords)
+        important_words = LLR(back_total_words, back_word_count, input_total_words, input_word_count, confidence_level)
+        sentence_weights = {} # keep track of weight of all sentences
+        for i in range(len(all_sentences)): 
+            # keep track of length and weight of each sentence
+            sentence_weights[i] = (len(all_sentences[i]), calculate_weight(all_sentences[i], important_words))
+        ordered_sentences = sorted(sentence_weights.items(), key=lambda x: x[1][1])
+        selected_sentences_indices = []
+        curr_length = 0
+        while True:
+            if ordered_sentences == []:
+                break
+            chosen = ordered_sentences.pop()
+            if curr_length + chosen[1][0] <= summary_length:
+                selected_sentences_indices.append(chosen[0]) # index
+                curr_length += chosen[1][0]   # length
+            else:
+                break
+        
+        # Create the output_directory if it doesn't exist
+        os.makedirs(output_directory, exist_ok=True)
+        with open(os.path.join(output_directory, subdir), "w") as output:
+            for i in selected_sentences_indices:
+                output.write(" ".join(all_sentences[i]) + "\n")
+        
---
+++ D3/src/make_LLR_background.py
@@ -0,0 +1,42 @@
+'''
+make the background_corpus based on files of /dropbox/23-24/575x/TAC_2010_KBP_Source_Data/data/2009/nw/cna_eng (19970921-20070609)
+'''
+import sys
+import xml.etree.ElementTree as ET
+import nltk
+from pathlib import Path
+
+
+def process(input_directory, output_file):
+    with open(output_file, 'w') as output:
+        for file_path in input_directory.glob('**/*'):
+            if file_path.is_file():
+                with open(file_path, 'r') as file:
+                    xml_data = file.read()
+                    
+                    # Parse the XML data
+                    root = ET.fromstring(xml_data)           
+                        
+                    # Find the BODY element
+                    body_element = root.find('BODY')      
+                    
+                    first_paragraph_skipped = False # skip information like "Tokyo,  Oct.  5  (CNA) (By Sofia Wu)"
+                    for paragraph in body_element.find('TEXT').findall('P'):
+                        if not first_paragraph_skipped:
+                            first_paragraph_skipped = True
+                            continue  
+                        paragraph = paragraph.text.replace('\n', ' ')
+                        sentences = nltk.sent_tokenize(paragraph)
+                        for sentence in sentences:
+                            # Tokenize the sentence into a list of words
+                            tokenized_words = " ".join(nltk.word_tokenize(sentence))
+                            output.write(tokenized_words + "\n")
+                    output.write("\n\n\n")
+                
+                
+if __name__ == "__main__":   
+    output_file = sys.argv[1]
+    input_directory = Path('./cna_eng')        
+    process(input_directory, output_file)
+    
+    
