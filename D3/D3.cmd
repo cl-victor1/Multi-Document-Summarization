075b22dae8ec862286b23c39e344885887f99401
cl-victor1 <cheng_long_@126.com>
2024-02-05 22:51:31 +0800

    update D3

--- D3/D3.cmd
+++ D3/D3.cmd
@@ -3,7 +3,7 @@ error = content_select.error
 log = content_select.log
 getenv = true
 notification = complete
-arguments = "D3 D2/outputs/devtest_output"
+arguments = "D3/data/back_corpus_file D2/outputs/devtest_output"
 request_memory = 512
 request_GPUs = 1
 Queue
