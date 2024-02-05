2dce398ea526890a2cd475408d16d39e614c86bd
Vicky(Xinyi) Xiang <xinyixiang1129@gmail.com>
2024-02-05 12:33:24 -0600

    add eval python file and initial output files

--- D3/src-eval/rouge.py
+++ D3/src-eval/rouge.py
@@ -1,27 +1,41 @@
 """
-This is the sample code for implementing ROUGE using the package.
+This is the sample code for implementing ROUGE using the package rouge-score (https://pypi.org/project/rouge-score/).
 """
-from rouge import Rouge
+import os
+import sys
 
-def calculate_rouge(reference, hypothesis):
-    rouge = Rouge()
-    scores = rouge.get_scores(hypothesis, reference)
-    return scores
+from rouge_score import rouge_scorer
 
-# Example usage
-reference_summary = "This is the reference summary."
-hypothesis_summary = "This is the hypothesis summary."
+def rouge(target_file_dir, prediction_file_dir, output_filename):
+    target_filenames = os.listdir(target_file_dir)
+    prediction_filenames = os.listdir(prediction_file_dir)
 
-rouge_scores = calculate_rouge(reference_summary, hypothesis_summary)
 
-print("ROUGE-1 Precision:", rouge_scores[0]['rouge-1']['p'])
-print("ROUGE-1 Recall:", rouge_scores[0]['rouge-1']['r'])
-print("ROUGE-1 F1 Score:", rouge_scores[0]['rouge-1']['f'])
+    for filename in target_filenames:
+        target_filename = str(target_file_dir + filename)
+        if target_file_dir.startswith("LLR"):
+            prediction_filename = str(prediction_file_dir + "/" + filename)
+        else:
+            prediction_filename = str(prediction_file_dir + filename)
 
-print("\nROUGE-2 Precision:", rouge_scores[0]['rouge-2']['p'])
-print("ROUGE-2 Recall:", rouge_scores[0]['rouge-2']['r'])
-print("ROUGE-2 F1 Score:", rouge_scores[0]['rouge-2']['f'])
+        scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
 
-print("\nROUGE-L Precision:", rouge_scores[0]['rouge-l']['p'])
-print("ROUGE-L Recall:", rouge_scores[0]['rouge-l']['r'])
-print("ROUGE-L F1 Score:", rouge_scores[0]['rouge-l']['f'])
+        target_file = open(target_filename, "r")
+        prediction_file = open(prediction_filename, "r")
+
+        target_sentences_list = target_file.readlines()
+        target_summary = "".join(target_sentences_list)
+        prediction_sentences_list = prediction_file.readlines()
+        prediction_summary = " ".join(prediction_sentences_list)
+
+        output_file = open(output_filename, "a+")
+        score = scorer.score(target_summary, prediction_summary)
+        output_file.writelines("ROUGE for " + str(target_filename)[-17:] + " is: " + str(score) + "\n")
+
+
+if __name__ == "__main__":
+    target_file_directory = sys.argv[1]
+    prediction_file_directory = sys.argv[2]
+    output_filename = sys.argv[3]
+
+    rouge(target_file_directory, prediction_file_directory, output_filename)
