8a8a0f5f3b080d5b634c06f1f2c6d054e3e95d15
cl-victor1 <cheng_long_@126.com>
2024-02-06 00:07:28 +0800

    add initial evaluation scripts
    average R, P and F (and conf int) are to be added to the output

--- D3/src/content_select.sh
+++ D3/src/content_select.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 pip install rouge-score
-# $1 $2 are 
+# make background corpus for LLR
 /nopt/dropbox/23-24/570/envs/570/bin/python ./src/make_LLR_background.py $1 $2
-
-/nopt/dropbox/23-24/570/envs/570/bin/python ./src/LLR.py $1 $3 $4 $5 $6 $7
+# with and without stopwords
+/nopt/dropbox/23-24/570/envs/570/bin/python ./src/LLR.py $1 $3 $4 $8 $9 $5
+/nopt/dropbox/23-24/570/envs/570/bin/python ./src/LLR.py $1 $3 $6 $8 $9 $7
 
 /nopt/dropbox/23-24/570/envs/570/bin/python ./src/sumBasic.py ??
 
-/nopt/dropbox/23-24/570/envs/570/bin/python ./src/lexrank.py ??
+# /nopt/dropbox/23-24/570/envs/570/bin/python ./src/lexrank.py ??
 #/dropbox/18-19/573/Data/models/devtest/
+/nopt/dropbox/23-24/570/envs/570/bin/python ./src/evaluate.py $15 $16
+/nopt/dropbox/23-24/570/envs/570/bin/python ./src/evaluate.py $15 $16
 /nopt/dropbox/23-24/570/envs/570/bin/python ./src/evaluate.py $15 $16
