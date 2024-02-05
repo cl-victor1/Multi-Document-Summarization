94cf3cc99f6631e9b7443828e7a326d43875deaf
cl-victor1 <cheng_long_@126.com>
2024-02-06 00:07:28 +0800

    update D3

--- D3/D3.cmd
+++ D3/D3.cmd
@@ -3,7 +3,7 @@ error = content_select.error
 log = content_select.log
 getenv = true
 notification = complete
-arguments = "D3/data/back_corpus_file D2/outputs/devtest_output"
+arguments = "D3/data/back_corpus_file D3/data/back_corpus D2/outputs/devtest_output D3/outputs/LLR_summaries_no_stopwords_0.05 0 D3/outputs/LLR_summaries_stopwords_0.05 1 0.05 100"
 request_memory = 512
 request_GPUs = 1
 Queue
