1e869805763c20a75dc703a9c97f6ad0fda0b23b
Catherine Qingyun Wang <qywang@dryas.ling.washington.edu>
2024-02-05 21:10:31 -0800

    updated ROUGE scores

--- D3/src/evaluation.py
+++ D3/src/evaluation.py
@@ -30,29 +30,39 @@ def main():
     hypothesis_summary_filenames = []
     
     model_summaries_A = []
-    model_summaries_B = []
-    model_summaries_C = []
-    model_summaries_D = []
+    # model_summaries_B = []
+    # model_summaries_C = []
+    # model_summaries_D = []
+
+
+    model_summaries = {}
 
 
     for file in os.listdir(hypothesis_summary_dir):
-        hypothesis_summary_filenames.append(hypothesis_summary_dir+file)
+        hypothesis_summary_filenames.append(hypothesis_summary_dir+"/"+file)
 
     for file in os.listdir(model_summary_dir):
         if file.endswith('.A'):
-            model_summaries_A.append(model_summary_dir+file)
-        if file.endswith('.B'):
-            model_summaries_B.append(model_summary_dir+file)
-        if file.endswith('.C'):
-            model_summaries_C.append(model_summary_dir+file)
-        if file.endswith('.D'):
-            model_summaries_D.append(model_summary_dir+file)
+            model_summaries_A.append(model_summary_dir+"/"+file)
+        # if file.endswith('.B'):
+        #     model_summaries_B.append(model_summary_dir+file)
+        # if file.endswith('.C'):
+        #     model_summaries_C.append(model_summary_dir+file)
+        # if file.endswith('.D'):
+        #     model_summaries_D.append(model_summary_dir+file)
+            
+        topicID = file.split("-")[0]
+        if topicID not in model_summaries:
+            model_summaries[topicID] = []
+        model_summaries[topicID].append(model_summary_dir+"/"+file)
+
+    # print(model_summaries)
 
 
     out_dict = [] 
     fields = ['topicID', 'ROUGE-1-avgP', 'ROUGE-1-avgR', 'ROUGE-1-avgF', 'ROUGE-2-avgP', 'ROUGE-2-avgR', 'ROUGE-2-avgF', 'ROUGE-L-avgP', 'ROUGE-L-avgR', 'ROUGE-L-avgF']
 
-    name = hypothesis_summary_dir.split("/")[-2]
+    name = hypothesis_summary_dir.split("/")[-1]
 
     outfile_name = f'rouge_output_{name}.csv'
 
@@ -60,30 +70,39 @@ def main():
         writer = csv.DictWriter(outfile, fieldnames=fields)
         writer.writeheader()
 
-        for i in range(len(model_summaries_A)):
+        for i in range(len(hypothesis_summary_filenames)):
             row = {}
-            row['topicID'] = hypothesis_summary_filenames[i].split("/")[-1]
+            filename = hypothesis_summary_filenames[i].split("/")[-1]
+            topicID = filename.split("-")[0]
+            row['topicID'] = topicID
+
+            model_summs = model_summaries[topicID]
+            scores = []
+            for summary in model_summs:
+                scores.append(get_rouge_score(hypothesis_summary_filenames[i], summary)) 
+            # scoreA = get_rouge_score(model_summaries_A[i], hypothesis_summary_filenames[i])
+            # scoreB = get_rouge_score(model_summaries_B[i], hypothesis_summary_filenames[i])
+            # scoreC = get_rouge_score(model_summaries_C[i], hypothesis_summary_filenames[i])
+            # scoreD = get_rouge_score(model_summaries_D[i], hypothesis_summary_filenames[i])
+
+
+            # scores = [scoreA,scoreB,scoreC,scoreD]
+
+
             
-            scoreA = get_rouge_score(model_summaries_A[i], hypothesis_summary_filenames[i])
-            scoreB = get_rouge_score(model_summaries_B[i], hypothesis_summary_filenames[i])
-            scoreC = get_rouge_score(model_summaries_C[i], hypothesis_summary_filenames[i])
-            scoreD = get_rouge_score(model_summaries_D[i], hypothesis_summary_filenames[i])
+            print(f'ROUGE for {filename} is:', scores[0])
 
-            scores = [scoreA,scoreB,scoreC,scoreD]
+            row['ROUGE-1-avgP'] = sum([score['rouge1'][0] for score in scores]) / len(scores)
+            row['ROUGE-2-avgP'] = sum([score['rouge2'][0] for score in scores]) / len(scores)
+            row['ROUGE-L-avgP'] = sum([score['rougeL'][0] for score in scores]) / len(scores)
 
-            print(f'ROUGE for {hypothesis_summary_filenames[i]} is:', scoreA)
-
-            row['ROUGE-1-avgP'] = sum([score['rouge1'][0] for score in scores]) / 4 
-            row['ROUGE-2-avgP'] = sum([score['rouge2'][0] for score in scores]) / 4 
-            row['ROUGE-L-avgP'] = sum([score['rougeL'][0] for score in scores]) / 4 
-
-            row['ROUGE-1-avgR'] = sum([score['rouge1'][1] for score in scores]) / 4 
-            row['ROUGE-2-avgR'] = sum([score['rouge2'][1] for score in scores]) / 4 
-            row['ROUGE-L-avgR'] = sum([score['rougeL'][1] for score in scores]) / 4 
+            row['ROUGE-1-avgR'] = sum([score['rouge1'][1] for score in scores]) / len(scores)
+            row['ROUGE-2-avgR'] = sum([score['rouge2'][1] for score in scores]) / len(scores)
+            row['ROUGE-L-avgR'] = sum([score['rougeL'][1] for score in scores]) / len(scores)
             
-            row['ROUGE-1-avgF'] = sum([score['rouge1'][2] for score in scores]) / 4 
-            row['ROUGE-2-avgF'] = sum([score['rouge2'][2] for score in scores]) / 4 
-            row['ROUGE-L-avgF'] = sum([score['rougeL'][2] for score in scores]) / 4 
+            row['ROUGE-1-avgF'] = sum([score['rouge1'][2] for score in scores]) / len(scores)
+            row['ROUGE-2-avgF'] = sum([score['rouge2'][2] for score in scores]) / len(scores)
+            row['ROUGE-L-avgF'] = sum([score['rougeL'][2] for score in scores]) / len(scores)
 
             out_dict.append(row)
 
