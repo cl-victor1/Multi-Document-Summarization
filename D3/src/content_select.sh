af7e071d3db7efa48fe55ec82fa93d478ecd32c3
cl-victor1 <cheng_long_@126.com>
2024-02-06 11:27:54 +0800

    update .sh

--- D3/src/content_select.sh
+++ D3/src/content_select.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 pip install rouge-score
 
-# make background corpus for LLR
-python ./src/make_LLR_background.py D3/data/back_corpus_file D3/data/back_corpus
-# with and without stopwords
-python ./src/LLR.py D3/data/back_corpus_file D2/outputs/devtest_output D3/outputs/LLR_summaries_no_stopwords_0.05 0.05 100 0
-python ./src/LLR.py D3/data/back_corpus_file D2/outputs/devtest_output D3/outputs/LLR_summaries_stopwords_0.05 0.05 100 1
-
-python ./src/sumBasic.py ？
-python ./src/sumBasic.py ？
-python ./src/sumBasic.py ？
-
-python ./src/lexrank.py D2/outputs/devtest_output D3/outputs/lexrank_summaries 100
-
-python ./src/evaluation.py D3/data/model_devtest D3/outputs/LLR_summaries_no_stopwords_0.05 > D3/results/rouge_scores_LLR.out
-python ./src/evaluate.py D3/data/model_devtest D3/outputs/LLR_summaries_stopwords_0.05 ???
-python ./src/evaluate.py D3/data/model_devtest D3/outputs/sumbasic_unigram_withStop_probability > D3/results/rouge_scores_sumBasic_unigram_withStop.out
+# LLR
+python ./make_LLR_background.py /D3/data/back_corpus_file /D3/data/back_corpus
+python ./LLR.py /D3/data/back_corpus_file /D2/outputs/devtest_output /D3/outputs/LLR_summaries_no_stopwords_0.05 0.05 100 0
+python ./LLR.py /D3/data/back_corpus_file /D2/outputs/devtest_output /D3/outputs/LLR_summaries_stopwords_0.05 0.05 100 1
+# sumBasic
+python ./sumBasic.py /D2/outputs -ry -1
+python ./sumBasic.py /D2/outputs -rn -1
+python ./sumBasic.py /D2/outputs -rn -2
+# lexrank
+python ./lexrank.py /D2/outputs/devtest_output /D3/outputs/LexRank_summary_length_5 5
+# evaluation
+python ./evaluation.py /D3/data/model_devtest /D3/outputs/LLR_summaries_no_stopwords_0.05 > /D3/results/rouge_scores_LLR_noStop.out
+python ./evaluation.py /D3/data/model_devtest /D3/outputs/LLR_summaries_stopwords_0.05 > /D3/results/rouge_scores_LLR_withStop.out
+python ./evaluation.py /D3/data/model_devtest /D3/outputs/sumbasic_unigram_withStop_probability > /D3/results/rouge_scores_sumBasic_unigram_withStop.out
+python ./evaluation.py /D3/data/model_devtest /D3/outputs/sumbasic_unigram_noStop_probability > /D3/results/rouge_scores_sumBasic_unigram_noStop.out
+python ./evaluation.py /D3/data/model_devtest /D3/outputs/sumbasic_bigram_withStop_probability > /D3/results/rouge_scores_sumBasic_bigram_withStop.out
+python ./evaluation.py /D3/data/model_devtest /D3/outputs/LexRank_summary_length_5 > /D3/results/rouge_scores_LexRank.out
