4d2f9efb7fff76d922499c33176713af845e0a4f
Sheng BI <shengbi@uw.edu>
2024-02-15 14:09:40 -0800

    Update info_ordering_entity_grid.sh

--- D4/src/info_ordering_entity_grid.sh
+++ D4/src/info_ordering_entity_grid.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # infor_ordering_entity_grid.py
-python ./info_ordering_entity_grid.py /D2/outputs/devtest_output /D4/outputs/sumbasic_summaries_no_stopwords /D4/outputs/io1/sumbasic_summaries_no_stopwords
-python ./info_ordering_entity_grid.py /D2/outputs/devtest_output /D4/outputs/sumbasic_summaries_stopwords /D4/outputs/io1/sumbasic_summaries_stopwords
+python ./info_ordering_entity_grid.py /D2/outputs/devtest_output /D4/outputs/sumbasic_summaries_no_stopwords /D4/outputs/io1/io_sumbasic_summaries_no_stopwords
+# python ./info_ordering_entity_grid.py /D2/outputs/devtest_output /D4/outputs/sumbasic_summaries_stopwords /D4/outputs/io1/io_sumbasic_summaries_stopwords
