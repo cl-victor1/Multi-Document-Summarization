9eb112618e9a08a2979a1550edbd16935fcbfc5d
Sheng BI <shengbi@uw.edu>
2024-02-14 17:56:33 -0800

    Update sumbasic.sh

--- D4/src/sumbasic.sh
+++ D4/src/sumbasic.sh
@@ -1,4 +1,4 @@
-
+#!/bin/sh
 
 python ./sumbasic.py /D2/outputs/devtest_output /D4/outputs/sumbasic_summaries_no_stopwords -ry -1 prob 5
 python ./sumbasic.py /D2/outputs/devtest_output /D4/outputs/sumbasic_summaries_stopwords -rn -1 prob 5
