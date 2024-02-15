f07fa0e3d07aeeefa56bb7a4267665b7c26794ef
Sheng BI <shengbi@uw.edu>
2024-02-15 05:49:20 -0800

    Update info_ordering_entity_grid.sh

--- D4/src/info_ordering_entity_grid.sh
+++ D4/src/info_ordering_entity_grid.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# LLR
-python ./info_ordering_entity_grid.py /D4/data/back_corpus_file /D2/outputs/devtest_output /D4/outputs/LLR_summaries_no_stopwords_0.05 0.05 100 0 5 0.8
-python ./info_ordering_entity_grid.py /D4/data/back_corpus_file /D2/outputs/devtest_output /D4/outputs/LLR_summaries_stopwords_0.05 0.05 100 1 5 0.8
+# infor_ordering_entity_grid.py
+python ./info_ordering_entity_grid.py /D2/outputs/devtest_output /D4/outputs/sumbasic_summaries_no_stopwords /D4/outputs/io1/sumbasic_summaries_no_stopwords
+python ./info_ordering_entity_grid.py /D2/outputs/devtest_output /D4/outputs/sumbasic_summaries_stopwords /D4/outputs/io1/sumbasic_summaries_stopwords
