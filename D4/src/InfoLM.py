41ab607dcfcbcc391d7cfe4a27c06167343970ff
vx <xinyixiang1129@gmail.com>
2024-02-18 21:42:44 -0600

    Auto stash before merge of "main" and "origin/main"

--- D4/src/InfoLM.py
+++ D4/src/InfoLM.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from torchmetrics.functional.text.infolm import infolm
 
+<<<<<<< Updated upstream
 def calc_avg_document_scores(target_file_dir, target_filename_unspecified, prediction_filename):
     target_filenames = os.listdir(target_file_dir)
     prediction_file = open(prediction_filename, "r")
@@ -66,6 +67,49 @@ def info_lm(target_file_dir, prediction_file_dir, method_id, output_csv_filename
         l_inf_list.append(str(avg_l_inf_score))
 
     result_dict = {"KL_divergence_scores": kl_list, "L1": l1_list, "L2": l2_list, "L_inf": l_inf_list}
+=======
+def info_lm(target_file_dir, prediction_file_dir, output_csv_filename):
+    prediction_filenames = os.listdir(prediction_file_dir)
+
+    kl_list, l1_list, l2_list, l_inf_list = [], [], [], []
+
+    for filename in prediction_filenames:
+        target_filename = str(target_file_dir + filename[:-1] + "A")
+        prediction_filename = str(prediction_file_dir + filename)
+
+        target_file = open(target_filename, "r")
+        prediction_file = open(prediction_filename, "r")
+
+        target_sentences_list = target_file.readlines()
+        target_summary = "".join(target_sentences_list)
+        prediction_sentences_list = prediction_file.readlines()
+        prediction_summary = " ".join(prediction_sentences_list)
+
+        kl_divergence_score = infolm(prediction_summary, target_summary, model_name_or_path='../data/bert_uncased_L-12_H-768_A-12',
+                                             idf=False, information_measure="kl_divergence")
+        l1_distance_score = infolm(prediction_summary, target_summary,
+                                            model_name_or_path='../data/bert_uncased_L-12_H-768_A-12',
+                                            idf=False, information_measure="l1_distance")
+        l2_distance_score = infolm(prediction_summary, target_summary,
+                                   model_name_or_path='../data/bert_uncased_L-12_H-768_A-12',
+                                   idf=False, information_measure="l2_distance")
+        l_inf_distance_score = infolm(prediction_summary, target_summary,
+                                   model_name_or_path='../data/bert_uncased_L-12_H-768_A-12',
+                                   idf=False, information_measure="l_infinity_distance")
+
+        print("Processing target file " + str(target_filename) + " and prediction file " + str(prediction_filename))
+        print(str(kl_divergence_score))
+        print(str(l1_distance_score))
+        print(str(l2_distance_score))
+        print(str(l_inf_distance_score))
+
+        kl_list.append(str(kl_divergence_score))
+        l1_list.append(str(l1_distance_score))
+        l2_list.append(str(l2_distance_score))
+        l_inf_list.append(str(l_inf_distance_score))
+
+    result_dict = {"KL_divergence_scores": kl_list, "L1": l1_list, "L2": l2_list, "L_inf":l_inf_list}
+>>>>>>> Stashed changes
     df = pd.DataFrame(result_dict)
     df.to_csv(str(output_csv_filename))
 
