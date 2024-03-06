3ae6f5359019246876bfddf1e6551354937d6014
cl-victor1 <cheng_long_@126.com>
2024-03-06 21:42:40 +0800

    cmd

--- D5/D5.cmd
+++ D5/D5.cmd
@@ -1,6 +1,6 @@
-executable = ./src/info_ordering.sh
-error = info_ordering.error
-log = info_ordering.log
+executable = ./src/content_realization.sh
+error = content_realization.error
+log = content_realization.log
 getenv = true
 notification = complete
 request_memory = 512
